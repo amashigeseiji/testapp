7ddad7d713d17654e80ce551560fd5a17591e4bb
amashigeseiji <amashige@tejimaya.com>
2013-03-19 03:43:54 +0900

    Data.class.php commit

--- Data.class.php
+++ Data.class.php
@@ -1,9 +1,38 @@
 <?php
-
-class DataClass extends BaseDataClass
+class Data
 {
-  public $id = '';
-  public $title = '';
-  public $body = '';
+  private $id;
+  private $title;
+  private $body;
   private $delkey = false;
+
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  public function setId($id)
+  {
+    $this->id = $id;
+  }
+
+  public function getTitle()
+  {
+    return $this->title;
+  }
+
+  public function setTitle($title)
+  {
+    $this->title = $title;
+  }
+
+  public function getBody()
+  {
+    return $this->body;
+  }
+
+  public function setBody($body)
+  {
+    $this->body = $body;
+  }
 }
