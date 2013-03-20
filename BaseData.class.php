a9702ce738c409812e8161dc33846ef8923ff0a0
amashigeseiji <amashige@tejimaya.com>
2013-03-20 16:17:51 +0900

    tetete

--- BaseData.class.php
+++ BaseData.class.php
@@ -3,13 +3,15 @@ class BaseData
 {
   private $basedata;
   private $path;
-  private $defaultpath = 'data/data.txt';
+  public $defaultpath = 'data/data.txt';
   public $errormessage = array();
+  private $errorlog = 'data/errolog';
 //  public $lastid = '';
 
   public function initialize()
   {
     $this->basedata = array();
+    $this->errormessage = array();
 
     $this->path = '';
     if (null == $this->path)
@@ -17,12 +19,21 @@ class BaseData
      $this->setPath($this->defaultpath);
     }
 
-    $lines = array();
-    $lines = file($this->path);
-
-    for ($i = 0; $i <= count($lines) -1; $i++)
+    if ( file_exists($this->path) )
     {
-      $this->basedata[$i] = explode(",", $lines[$i]);
+      //filepermissionの確認.775に設定する
+      if (substr(sprintf('%o',fileperms($this->path)),-4) != 0775)
+      {
+        chmod($this->path,0775);
+      }
+
+      $lines = array();
+      $lines = file($this->path);
+
+      for ($i = 0; $i <= count($lines) -1; $i++)
+      {
+        $this->basedata[$i] = explode(",", $lines[$i]);
+      }
     }
     //var_dump($this->basedata); exit;
   }
@@ -31,12 +42,16 @@ class BaseData
   {
     if (file_exists($file) && is_dir($file))
     {
-      $this->errormessage[] .= $file . " is directory.";
+      $this->errormessage = array();
+      $this->errormessage[] .= '[' . __method__ . '] file:' . $file . " is directory.";
+      $this->errorLog();
       $this->path = $this->defaultpath;
     }
-    elseif(!file_exists($file))
+    elseif (!file_exists($file))
     {
-      $this->errormessage[] .= $file . " is not exist.";
+      $this->errormessage = array();
+      $this->errormessage[] .= '[' . __method__ . '] file:' . $file . " not exist.";
+      $this->errorLog();
       $this->path = $this->defaultpath;
     }
     else
@@ -48,15 +63,26 @@ class BaseData
   public function getIds()
   {
     $ids = array();
-    if ( $this->basedata[0][0] == $this->basedata[1][0])
+    if ( !empty($this->basedata) )
     {
-      $ids[0] = $this->basedata[0][0];
-    }
-    for ( $i = 1; $i <= count($this->basedata) -1; $i++)
-    {
-      if ( $this->basedata[$i][0] != null && $this->basedata[$i][0] != $this->basedata[$i-1][0] )
+      //TODO もっと綺麗に書けるはず…
+      if ( count($this->basedata) == 1 )
       {
-        $ids[] .= $this->basedata[$i][0];
+        $ids[0] = $this->basedata[0][0];
+
+        return $ids;
+      }
+
+      if ( $this->basedata[0][0] == $this->basedata[1][0])
+      {
+        $ids[0] = $this->basedata[0][0];
+      }
+      for ( $i = 1; $i <= count($this->basedata) -1; $i++)
+      {
+        if ( $this->basedata[$i][0] != null && $this->basedata[$i][0] != $this->basedata[$i-1][0] )
+        {
+          $ids[] .= $this->basedata[$i][0];
+        }
       }
     }
 
@@ -80,7 +106,16 @@ class BaseData
 
   public function getLastId()
   {
-    return max($this->getIds());
+    //$idsに値が無い場合は0を返す
+    $ids = $this->getIds();
+    if ( !empty($ids) )
+    {
+      return max($ids);
+    }
+    else
+    {
+      return 0;
+    }
   }
 
   public function getTitleById($id)
@@ -95,7 +130,6 @@ class BaseData
       }
     }
 
-    $this->errormessage[] .= $id. 'is null.';
     return null;
   }
 
@@ -122,7 +156,6 @@ class BaseData
     }
     else
     {
-      $this->errormessage[] .= $id. 'is null.';
       return null;
     }
   }
@@ -130,11 +163,25 @@ class BaseData
   //title データが body より先にくる想定
   public function writeTitle($input)
   {
-    $fp = fopen($this->path, "a");
-    fwrite($fp, $this->getLastId() + 1 . ',' . 'title,' . $input . "\n");
-    fclose($fp);
-    //データの更新
-    $this->initialize();
+    //書き込み成功時にはtrue,失敗時にはfalseを返す
+    $lastid = $this->getLastId();
+    if ((file_exists($this->path)) && ($fp = fopen($this->path, "a")))
+    {
+      fwrite($fp, $lastid + 1 . ',' . 'title,' . $input . "\n");
+      fclose($fp);
+      //データの更新
+      $this->initialize();
+
+      return true;
+    }
+    else
+    {
+      $this->errormessage = array();
+      $this->errormessage[] .= '[' . __method__ . '] file:' . $this->path . ' can not open.';
+      $this->errorLog();
+
+      return false;
+    }
   }
 
   //title のデータが存在することを前提にする(titleよりもbodyをさきに書き込むとid順がくずれる)
@@ -151,18 +198,34 @@ class BaseData
     $this->initialize();
   }
 
+  public function writeDate()
+  {
+    $fp = fopen($this->path, "a");
+    fwrite($fp, $this->getLastId() . ',' . 'date,' . date("Y-m-d H:i:s") . "\n");
+    fclose($fp);
+  }
+
   public function delete($id)
   {
-    $data = file($this->path);
-    $deleted = preg_grep("/^$id,/",$data,PREG_GREP_INVERT);
-    $fp = fopen($this->path,"w");
-    foreach ($deleted as $key => $value)
+    if ( $this->isData($id) == true )
     {
-      fwrite($fp,$deleted[$key]);
-    }
-    fclose($fp);
+     $data = file($this->path);
+     $deleted = preg_grep("/^$id,/",$data,PREG_GREP_INVERT);
+     $fp = fopen($this->path,"w");
+     foreach ($deleted as $key => $value)
+     {
+       fwrite($fp,$deleted[$key]);
+     }
+     fclose($fp);
 
-    $this->initialize();
+     $this->initialize();
+    }
+    else
+    {
+      $this->errormessage = array();
+      $this->errormessage[] .= '[' . __method__ . '] id:' . $id . ' no data.';
+      $this->errorLog();
+    }
   }
 
   public function editTitle($id,$input)
@@ -187,6 +250,19 @@ class BaseData
       $obj->setBody($this->getBodyById($id));
 
       return $obj;
+    }
+  }
+
+  public function errorLog()
+  {
+    if ( isset($this->errormessage) )
+    {
+      $fp = fopen($this->errorlog,"a");
+      foreach ( $this->errormessage as $key => $value )
+      {
+        fwrite($fp,date("Y-m-d H:i:s") . " " . $this->errormessage[$key] . "\n");
+      }
+      fclose($fp);
     }
   }
 
