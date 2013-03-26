01b83a586cc1c2885bec9cdc93382367794c349d
amashigeseiji <amashige@tejimaya.com>
2013-03-27 02:29:10 +0900

    テンプレートファイルの編集

--- template/auth.php
+++ template/auth.php
@@ -1,20 +1,39 @@
+<!DOCTYPE>
 <html>
+<head>
+</head>
 <body>
+
+<?php if(null == $this->user): ?>
   <form action="#" method="post">
+
     <table>
       <tr>
         <th>name</th>
         <td><input type="text" name="name"></td>
       </tr>
+
       <tr>
         <th>password</th>
         <td><input type="password" name="password"></td>
       </tr>
+
       <tr>
         <td>
           <input type="submit" value="送信" />
         </td>
       </tr>
+
     </table>
   </form>
 
+  <?php else: ?>
+  <?php echo 'login:'.$this->user->getName() ?>
+    <form action="#" method="post" >
+      <input type="submit" name="logout" value="logout" />
+    </form>
+    <a href="<?php $_SERVER['PHP_SELF'] ?>">test</a>
+
+<?php endif; ?>
+</body>
+</html>
