efc14c2797436aacc2e7a83488b3a968e4cb8c2e
amashigeseiji <amashige@tejimaya.com>
2013-03-26 23:16:19 +0900

    いろいろ追加

---
+++ user/baseuser.class.php
@@ -0,0 +1,150 @@
+<?php
+class BaseUser
+{
+  private
+    $baseusers,
+    $path = 'data/user.txt';
+
+  public
+    $authusers = array(),
+    $message   = array(
+        'auth' => '',
+      ),
+    $user;
+
+
+  function __construct()
+  {
+    $this->setBaseUsers();
+  }
+
+  public function setBaseUsers()
+  {
+    $baseusers = array();
+    $lines = file($this->path);
+    foreach ( $lines as $key => $value )
+    {
+      $baseusers[$key] = explode(",",$lines[$key]);
+    }
+    $this->baseusers = $baseusers;
+  }
+
+  public function getBaseUsers()
+  {
+    return $this->baseusers;
+  }
+
+  public function getUserById($id)
+  {
+    foreach ( $this->baseusers as $key => $value )
+    {
+      if ( $this->baseusers[$key][0] == $id )
+      {
+        return $this->baseusers[$key];
+      }
+    }
+
+    return null;
+  }
+
+  public function isUser($id)
+  {
+    foreach ( $this->baseusers as $key => $value )
+    {
+      if ( $this->baseusers[$key][0] == $id )
+      {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public function getUserNameById($id)
+  {
+    var_dump($this->baseusers);
+    foreach ( $this->baseusers as $key => $value )
+    {
+      if ( $this->baseusers[$key][0] == $id )
+      {
+        return $this->baseusers[$key][1];
+      }
+    }
+
+    return null;
+  }
+
+  public function getUserIdByName($name)
+  {
+    foreach ( $this->baseusers as $key => $value )
+    {
+      if ( $this->baseusers[$key][1] == $name )
+      {
+        return $this->baseusers[$key][0];
+      }
+    }
+
+    return null;
+  }
+
+  public function getPasswordById($id)
+  {
+    foreach ( $this->baseusers as $key => $value )
+    {
+      if ( $this->baseusers[$key][0] == $id )
+      {
+        return $this->baseusers[$key][2];
+      }
+    }
+
+    return null;
+  }
+
+  public function getNumberOfUsers()
+  {
+  }
+
+  public function Authentication($name,$password)
+  {
+    if ( empty($name) )
+    {
+      $this->message['auth'] = '名前を入力してください。';
+      return false;
+    }
+    if ( empty($password) )
+    {
+      $this->message['auth'] = 'パスワードを入力してください。';
+      return false;
+    }
+
+    if ( $id = $this->getUserIdByName($name) );
+    {
+      if ( $this->getPasswordById($id) == $password )
+      {
+        $this->authusers[$id] = $this->createUser($id);
+        return true;
+      }
+
+      $this->message['auth'] = 'パスワードが違います。';
+      return false;
+    }
+
+    $this->message['auth'] = 'ユーザーが存在しません.';
+    return false;
+  }
+
+  public function createUser($id)
+  {
+    if ( $this->isUser($id) == true )
+    {
+      include_once('user.class.php');
+      $user = new User;
+      $user->setUserId($id);
+      $user->setName($id);
+      $user->setToken();
+
+      return $this->user = $user;
+    }
+  }
+
+}
